99015bd5ac05eeb17fee022a00439b028bec6539
Tom Howard <t.howard@sheffield.ac.uk>
2023-10-24 14:30:28 +0100

    turn code comments into annotations

--- docs/com2009/assignment1/part2/move_square.md
+++ docs/com2009/assignment1/part2/move_square.md
@@ -24,9 +24,59 @@ Moving in a square can be achieved by switching between two different movement s
 --8<-- "snippets/move_square.py"
 ```
 
+1. Import the `Twist` message for publishing velocity commands to `/cmd_vel`.
+2. Import the `Odometry` message, for use when subscribing to the `/odom` topic.
+3. Import the `euler_from_quaternion` function to convert orientation from quaternions (as provided in the `Odometry` message) to Euler angles (about [the principal axes](../../part2/#principal-axes)).
+4. Finally, import some useful mathematical operations (and `pi`), which you may find useful:
+
+    | Mathematical Operation | Python Implementation |
+    | :---: | :---: |
+    | $\sqrt{a+b}$ | `#!python sqrt(a+b)` |
+    | $a^{2}+(bc)^{3}$ | `#!python pow(a, 2) + pow(b*c, 3)` |
+    | $\pi r^2$ | `#!python pi * pow(r, 2)` |
+
+5. Obtain the relevant data from the `/odom` topic. For this, we need to know the robot's position and orientation in its environment, all of which is contained within the "pose" part of the `Odometry` message:
+
+    ```python
+    pose = topic_data.pose.pose
+    ```
+
+    From there, we can separate out the position and orientation parts:
+
+    ```python
+    position = pose.position
+    ```
+    
+    ```python
+    orientation = pose.orientation
+    ``` 
+
+6. Here we obtain the robot's **current** position coordinates.
+
+7. And here we obtain the robot's current orientation (in quaternions) and convert it to Euler angles (in radians) about [the principal axes](../../part2/#principal-axes), where:
+    * "roll" = <code>&theta;<sub>x</sub></code>
+    * "pitch" = <code>&theta;<sub>y</sub></code>
+    * "yaw" = <code>&theta;<sub>z</sub></code>
+
+8. We're only interested in `x`, `y` and <code>&theta;<sub>z</sub></code>, so we assign these to class variables `self.x`, `self.y` and `self.theta_z`, so that we can access them elsewhere within our `Square()` class.
+
+9. Sometimes, it can take a few moments for the first topic message to come through, and it's useful to know when that's happened so that you know you are dealing with actual topic data! Here, we're just setting a flag to `True` once the callback function has executed for the first time (i.e. the first topic message *has* been received).
+
+10. This might be useful in the `#!python main()` class method (below), to switch between turning and moving forwards...
+
+11. This node needs to **read** message data from one topic (`/odom`) and **write** messages on another (`/cmd_vel`), so we need to set up a subscriber and a publisher here accordingly. 
+
+12. Here, we define some variables that we can use to store relevant bits of odometry data while our node is running (and read it back to implement feedback control):
+    * `self.x`, `self.y` and `self.theta_z` will be used by the callback function to store the robot's **current** pose
+    * `self.x0`, `self.y0` and `self.theta_z0` can be used in the `main()` method to keep a record of where the robot **was** at a given moment in time (and determine how far it has moved since that point)
+
+13. Here we establish a `Twist` message, which we can populate with velocities and then publish to `/cmd_vel` within the `main()` method in order to make the robot move.
+
+14. Publish an empty `Twist` message to stop the robot (by default all velocities will be zero).
+
 ## Alternative Approach: Waypoint Tracking
 
-A square motion path can be fully defined by the co-ordinates of its four corners, and we can make the robot move to each of these corners one-by-one, using its odometry system to monitor its real-time position, and adapting linear and angular velocities accordingly.
+A square motion path can be fully defined by the coordinates of its four corners, and we can make the robot move to each of these corners one-by-one, using its odometry system to monitor its real-time position, and adapting linear and angular velocities accordingly.
 
 This is slightly more complicated, and you might want to wait until you have a bit more experience with ROS before tackling it this way (we'll also cover this in the COM2009 lecture course).
 
--- snippets/move_square.py
+++ snippets/move_square.py
@@ -1,77 +1,50 @@
 import rospy
-# import the Twist message for publishing velocity commands:
-from geometry_msgs.msg import Twist
-# import the Odometry message for subscribing to the odom topic:
-from nav_msgs.msg import Odometry
-# import the function to convert orientation from quaternions to angles:
-from tf.transformations import euler_from_quaternion
-# import some useful mathematical operations (and pi), which you may find useful:
-from math import sqrt, pow, pi
+from geometry_msgs.msg import Twist # (1)!
+from nav_msgs.msg import Odometry # (2)!
+from tf.transformations import euler_from_quaternion # (3)!
+from math import sqrt, pow, pi # (4)!
 
 class Square():
     def callback(self, topic_data: Odometry):
-        # obtain relevant topic data, e.g.: pose (position and orientation):
-        pose = topic_data.pose.pose
+        pose = topic_data.pose.pose # (5)!
         position = pose.position
         orientation = pose.orientation
 
-        # obtain the robot's position co-ords:
-        pos_x = position.x
+        pos_x = position.x # (6)!
         pos_y = position.y
 
-        # convert orientation co-ords to roll, pitch & yaw 
-        # (theta_x, theta_y, theta_z):
         (roll, pitch, yaw) = euler_from_quaternion(
             [orientation.x, orientation.y, orientation.z, orientation.w], "sxyz"
-        )
+        ) # (7)!
 
-        # We're only interested in x, y and theta_z
-        # so assign these to class variables (so that we can
-        # access them elsewhere within our Square() class):
-        self.x = pos_x
+        self.x = pos_x # (8)!
         self.y = pos_y
         self.theta_z = yaw
 
-        # If this is the first time that the callback_function has run
-        # (e.g. the first time a message has been received), then
-        # obtain a "reference position" (used to determine how far
-        # the robot has moved during its current operation)
-        if self.startup:
-            # don't initialise again:
-            self.startup = False
-            # set the reference position:
-            self.x0 = self.x
-            self.y0 = self.y
-            self.theta_z0 = self.theta_z
+        if self.first_message: # (9)!
+            self.first_message = True
 
     def __init__(self):
         node_name = "move_square"
-        # a flag if this node has just been launched
-        self.startup = True
+        self.first_message = False
+        self.turn = False # (10)!
 
-        # This might be useful in the main_loop() (to switch between 
-        # turning and moving forwards)
-        self.turn = False
-
-        # setup a '/cmd_vel' publisher and an '/odom' subscriber:
+        # (11)!
         self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
         self.sub = rospy.Subscriber("odom", Odometry, self.callback_function)
 
         rospy.init_node(node_name, anonymous=True)
         self.rate = rospy.Rate(10)  # hz
 
-        # define the robot pose variables and initialise them to zero:
-        # variables for the robot's "current position":
+        # (12)!
         self.x = 0.0
         self.y = 0.0
         self.theta_z = 0.0
-        # variables for a "reference position":
         self.x0 = 0.0
         self.y0 = 0.0
         self.theta_z0 = 0.0
 
-        # define a Twist message instance, to set robot velocities
-        self.vel = Twist()
+        self.vel = Twist() # (13)!
 
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdownhook)
@@ -79,9 +52,7 @@ class Square():
         rospy.loginfo(f"the {node_name} node has been initialised...")
 
     def shutdownhook(self):
-        # publish an empty twist message to stop the robot
-        # (by default all velocities will be zero):
-        self.pub.publish(Twist())
+        self.pub.publish(Twist()) # (14)!
         self.ctrl_c = True
 
     def main(self):
@@ -92,8 +63,7 @@ class Square():
 
             # publish whatever velocity command has been set in your code above:
             self.pub.publish(self.vel)
-            # maintain the loop rate @ 10 hz
-            self.rate.sleep()
+            self.rate.sleep() # maintain the loop rate @ 10 hz
 
 if __name__ == "__main__":
     node = Square()
